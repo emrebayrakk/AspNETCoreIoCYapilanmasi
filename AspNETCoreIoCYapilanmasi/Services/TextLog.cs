a795711031d677920ef87b6c99090495698d5943
Emre <eemmrree0@outlook.com>
2021-08-31 16:14:22 +0300

    Services klasöründe TextLog sınıfı oluşturuldu.

---
+++ AspNETCoreIoCYapilanmasi/Services/TextLog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNETCoreIoCYapilanmasi.Services
+{
+    public class TextLog
+    {
+        public void Log()
+        {
+
+        }
+    }
+}
