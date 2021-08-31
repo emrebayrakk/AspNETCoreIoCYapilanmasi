c66680b2c4347941ddcffb84c01bfa1e19c6660d
Emre <eemmrree0@outlook.com>
2021-08-31 16:51:46 +0300

    ConsoleLog sınıfına ILog arayüzü iplement edildi.

--- AspNETCoreIoCYapilanmasi/Services/ConsoleLog.cs
+++ AspNETCoreIoCYapilanmasi/Services/ConsoleLog.cs
@@ -3,20 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using AspNETCoreIoCYapilanmasi.Services.Interface;
 
 namespace AspNETCoreIoCYapilanmasi.Services
 {
-    public class ConsoleLog
+    public class ConsoleLog:ILog
     {
         public ConsoleLog(int a)
         {
 
         }
-        public void Log()
-        {
-
-           
-
-        }
+        
     }
 }
