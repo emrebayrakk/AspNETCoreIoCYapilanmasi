ce3d046721832c84da9e6237ee00dce7d9eccd24
Emre <eemmrree0@outlook.com>
2021-09-01 21:43:57 +0300

    Models Klasörünün içerisine Employee adında bir class oluşturduk.

---
+++ NetCoreFluentValidation/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreFluentValidation.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public int EmployeeLastName { get; set; }
+        public int EmployeeEmail { get; set; }
+    }
+}
