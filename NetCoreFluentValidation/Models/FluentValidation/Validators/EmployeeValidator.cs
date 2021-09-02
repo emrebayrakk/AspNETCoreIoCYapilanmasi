8b2c8284d965060117b7fd61e21dd38f618297fe
Emre <eemmrree0@outlook.com>
2021-09-02 16:19:57 +0300

    Model içerisinde Validators klasörünün içinde EmployeValidator sınıfı oluşturdu.

---
+++ NetCoreFluentValidation/Models/FluentValidation/Validators/EmployeeValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace NetCoreFluentValidation.Models.FluentValidation.Validators
+{
+    public class EmployeeValidator:AbstractValidator<Employee>
+    {
+        public EmployeeValidator()
+        {
+            RuleFor(x => x.EmployeeName).NotEmpty().WithMessage("Adı Boş Geçemezsiniz");
+
+            RuleFor(x => x.EmployeeLastName).NotEmpty().WithMessage("Soyadını Boş Geçemezsiniz");
+
+            RuleFor(x => x.EmployeeEmail).NotEmpty().WithMessage("Mail Boş Geçemezsiniz");
+        }
+    }
+}
